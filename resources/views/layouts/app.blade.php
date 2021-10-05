12642dd97703f7dbe790e21b0c380e478fa6981c
James Darlington <jdarlington78298@gmail.com>
2021-10-05 12:08:36 +0100

    Project recovered.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,6 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,7 +22,7 @@
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
-                <div class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
